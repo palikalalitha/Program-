aeae18f7450c319a8effa8f20c3c45dd677aa03e
lalitha <30374099+palikalalitha@users.noreply.github.com>
2020-05-13 12:23:03 +0530

    Create bit1.js

---
+++ bits/bit1.js
@@ -0,0 +1,56 @@
+user clikcs on the chnage title
+
+
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { inject, Provider, observer } from "mobx-react";
+import { observable, action } from "mobx";
+
+@inject("appStore")
+@observer
+class Message extends Component {
+  message;
+  constructor(props) {
+    super(props);
+    this.message = this.props.appStore.message;
+  }
+
+  onChangeTitle = () => {
+    const { onChangeTitle } = this.props.appStore;
+    onChangeTitle("Hi");
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Message title: {this.message.title}</p>
+        <button onClick={this.onChangeTitle}>Change title</button>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider appStore={appStore}>
+        <Message />
+      </Provider>
+    );
+  }
+}
+
+class AppStore {
+  @observable message = {
+    title: "Hello",
+  };
+
+  @action.bound
+  onChangeTitle(title) {
+    this.message.title = title;
+  }
+}
+
+const appStore = new AppStore();
+
+render(<App />, document.getElementById("root"));

a81741351af06e19791079e77374dc3f2ae69868
lalitha <30374099+palikalalitha@users.noreply.github.com>
2020-05-13 12:24:23 +0530

    Create bit2.js

---
+++ bits/bit2.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class Counter extends Component {
+  state = {
+    count: 2,
+  };
+  updateCount = () => {
+    this.setState({
+      count: 2,
+    });
+  };
+
+  render() {
+    console.log("render Counter");
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.updateCount}>Update count</button>
+      </div>
+    );
+  }
+}
+render(<Counter />, document.getElementById("root"));
